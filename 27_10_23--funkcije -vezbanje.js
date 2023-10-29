6faa5c933f58bfd8afafd834d36f5b0f711faac7
Darth_Koppa91 aka Aleks Koppa <94325606+Darthkoppa91@users.noreply.github.com>
2023-10-29 23:47:00 +0100

    29.10/practice/

---
+++ 27_10_23--funkcije -vezbanje.js
@@ -0,0 +1,29 @@
+
+//Napraviti funkciju koja izbacuje duplikate iz niza brojeva.//
+
+
+
+removeDuplicate([1,3,3,3,1,5,6,7,8,1]);
+ // = [1, 3, 5, 6, 7, 8]//
+
+ function removeDup(array, f) {
+
+    var array=[];
+    for (var i =0; i <array.length; i++) {
+        var element = array[i];
+        
+        console.log(element)
+        if(isItInArray(array,array[i])){
+
+        }
+    }
+
+
+ }
+ function isItInArray(array, item) {
+    for(let i = 0; i<array.length; i++) {
+        
+    }
+ } 
+
+
