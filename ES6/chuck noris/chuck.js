8a0fd355fdabc5d263075e9a3eccbaee7cf39ec7
Darthkoppa91 <nowhereextreme@gmail.com>
2023-11-20 13:20:38 +0100

    chuck norris vicevi/fetchovanje

--- ES6/chuck noris/chuck.js
+++ ES6/chuck noris/chuck.js
@@ -1,28 +1,27 @@
-const img = document.querySelector("img");
+const paragraf = document.querySelector("p");
 // ako je klasa ide tacka ispred img ako je id onda taraba //
 // console.log(img);
 
 const btn = document.querySelector("button");
 // console.log(btn)
 
-  function fetchDog() {
+  function fetchFraze() {
     try {
-        fetch("https://dog.ceo/api/breeds/image/random").then((response) => {
+        fetch("https://api.chucknorris.io/jokes/random").then((response) => {
         //   if (response.status === 200) {
         //     throw new Error("greska");
         //   }
           return response.json();
     
-        }).then((data) => changeDogImage(data));
+        }).then((data) => changeChuckQuote(data));
     
       } catch (err) {
         console.log(err);
       }
   }
-  function changeDogImage({status, message}) {
-    // console.log(message);
-    img.setAttribute("src", message);
+  function changeChuckQuote({id,value}) {
+     console.log(id);
+     
+    paragraf.innerHTML = value;
  }
- btn.addEventListener("click", fetchDog);
- 
- 
+ btn.addEventListener("click", fetchFraze);
